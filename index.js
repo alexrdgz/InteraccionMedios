175c1e3f0d694b65f5c65a0262366ee89790e902
alexrdgz <alexrodriguez.ieme@gmail.com>
2023-04-18 03:17:34 -0600

    interaccion-medios

--- index.js
+++ index.js
@@ -6,6 +6,62 @@ navToggle.addEventListener("click", (event) => {
   navMenu.classList.toggle("nav-menu_visible");
 });
 
+
+// function cambiarImagen() {
+//   var hoy = new Date().getDay();
+//   var imagen = document.getElementById("martes");
+
+//   if (hoy === 0) {
+//     imagen = document.getElementById("domingo");
+//   } else if (hoy === 1) {
+//     imagen = document.getElementById("lunes");
+//   } else if (hoy === 2) {
+//     imagen = document.getElementById("martes");
+//   } else if (hoy === 3) {
+//     imagen = document.getElementById("miercoles");
+//   } else if (hoy === 4) {
+//     imagen = document.getElementById("jueves");
+//   } else if (hoy === 5) {
+//     imagen = document.getElementById("viernes");
+//   } else if (hoy === 6) {
+//     imagen = document.getElementById("sabado");
+//   }
+
+//   imagen.style.display = "block";
+// }
+
+
+
+
+function cambiarImagen() {
+  var hoy = new Date().getDay();
+  var imagen = document.getElementById('martes');
+
+  if (hoy === 0) {
+    imagen.src = "./images/domingo.webp";
+    imagen.alt = "Programacion del domingo";
+  } else if (hoy === 1) {
+    imagen.src = "./images/Lunes.jpg";
+    imagen.alt = "Programacion del lunes";
+  } else if (hoy === 2) {
+    imagen.src = "./images/martes.jpg";
+    imagen.alt = "Programacion del martes";
+  } else if (hoy === 3) {
+    imagen.src = "./images/miercoles.jpg";
+    imagen.alt = "Programacion del miércoles";
+  } else if (hoy === 4) {
+    imagen.src = "./images/jueves.jpg";
+    imagen.alt = "Programacion del jueves";
+  } else if (hoy === 5) {
+    imagen.src = "./images/viernes.jpg";
+    imagen.alt = "Programacion del viernes";
+  } else if (hoy === 6) {
+    imagen.src = "./images/sabado.jpg";
+    imagen.alt = "Programacion del sábado";
+  }
+  imagen.style.display = "block";
+}
+
 const sliderWrapper = document.querySelector(".slider-wrapper");
 const sliderPrev = document.querySelector(".slider-prev");
 const sliderNext = document.querySelector(".slider-next");
