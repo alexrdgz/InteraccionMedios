d93bc19f6ab3558c6bbce8e1e215e55f4ec1a36b
alexrdgz <alexrodriguez.ieme@gmail.com>
2023-04-17 20:04:42 -0600

    interaccion-medios

--- index.js
+++ index.js
@@ -1,18 +1,80 @@
-// let offset = 0;
-// const sliderLine = document.querySelector('.slider-line');
+const navToggle = document.querySelector(".nav-toggle");
+const navMenu = document.querySelector(".nav-menu");
 
-// document.querySelector('.slider-next').addEventListener('click', function(){
-//   offset = offset + 100;
-//   if (offset > 200) {
-//     offset = 0;
-//   }
-//   sliderLine.style.left = -offset + '%';
-// });
+navToggle.addEventListener("click", () => {
+  navMenu.classList.toggle("nav-menu_visible");
+});
 
-// document.querySelector('.slider-prev').addEventListener('click', function(){
-//   offset = offset - 100;
-//   if (offset < 0) {
-//     offset = 200;
-//   }
-//   sliderLine.style.left = -offset + '%';
-// });
+const sliderWrapper = document.querySelector(".slider-wrapper");
+const sliderPrev = document.querySelector(".slider-prev");
+const sliderNext = document.querySelector(".slider-next");
+let currentPosition = 0;
+
+function moveSlider(position) {
+  currentPosition = position;
+  sliderWrapper.style.left = `-${currentPosition}00%`;
+}
+
+sliderPrev.addEventListener("click", () => {
+  if (currentPosition > 0) {
+    currentPosition--;
+    moveSlider(currentPosition);
+  }
+});
+
+sliderNext.addEventListener("click", () => {
+  if (currentPosition < 2) {
+    currentPosition++;
+    moveSlider(currentPosition);
+  }
+});
+
+setInterval(() => {
+  if (currentPosition < 2) {
+    currentPosition++;
+    moveSlider(currentPosition);
+  } else {
+    currentPosition = 0;
+    moveSlider(currentPosition);
+  }
+}, 5000);
+
+
+
+
+const sliderImages = document.querySelectorAll('.slider-wrapper img');
+const prevButton = document.querySelector('.slider-prev');
+const nextButton = document.querySelector('.slider-next');
+
+
+const modal = document.querySelector('.modal');
+const modalContent = document.querySelector('.modal-content');
+const closeModal = document.querySelector('.close');
+
+
+let selectedIndex = 0;
+
+
+sliderImages.forEach((image, index) => {
+  image.addEventListener('click', () => {
+ 
+    selectedIndex = index;
+    modalContent.src = image.src;
+    modal.style.display = 'block';
+  });
+});
+
+
+closeModal.addEventListener('click', () => {
+  modal.style.display = 'none';
+});
+
+
+modal.addEventListener('click', (event) => {
+ 
+  if (event.target === modal) {
+    modal.style.display = 'none';
+  }
+});
+
+
