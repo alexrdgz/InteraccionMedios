f3c3fc82d0dd5b1419e18310c7cfbb29f53ca575
alexrdgz <alexrodriguez.ieme@gmail.com>
2023-04-28 01:02:25 -0600

    interaccion-medios

--- index.js
+++ index.js
@@ -52,7 +52,7 @@ var swiper = new Swiper(".mySwiper", {
   spaceBetween: 30,
   centeredSlides: true,
   autoplay: {
-    delay: 3000,
+    delay: 10000,
     disableOnInteraction: false,
   },
   pagination: {
