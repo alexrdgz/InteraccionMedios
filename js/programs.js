d4cff8a6f44e53d39e23dfd657931c9f27972734
alexrdgz <alexrodriguez.ieme@gmail.com>
2023-05-09 22:32:38 -0600

    interaccion-medios

---
+++ js/programs.js
@@ -0,0 +1,38 @@
+const navToggle = document.querySelector(".nav-toggle");
+const navMenu = document.querySelector(".nav-menu");
+const navLinks = document.querySelectorAll(".nav-link");
+
+navToggle.addEventListener("click", (event) => {
+  event.preventDefault();
+  navMenu.classList.toggle("nav-menu_visible");
+});
+
+navLinks.forEach((link) => {
+  link.addEventListener("click", () => {
+    navMenu.classList.remove("nav-menu_visible");
+    const href = link.getAttribute("href");
+    window.location.href = href;
+  });
+});
+
+
+
+function mostrarEnVivo() {
+  var ahora = new Date();
+  var dia = ahora.getDay();
+  var hora = ahora.getHours();
+  var minutos = ahora.getMinutes();
+
+  if ((dia == 4 || dia == 5) && hora == 20) {
+    document.getElementById("en-vivo").style.display = "block";
+  } else {
+    document.getElementById("en-vivo").style.display = "none";
+  }
+  if (dia == 5 && hora == 21 && minutos < 30) {
+    document.getElementById("en-vivo").style.display = "block";
+  } else {
+    document.getElementById("en-vivo").style.display = "none";
+  }
+}
+
+setInterval(mostrarEnVivo, 2000);
